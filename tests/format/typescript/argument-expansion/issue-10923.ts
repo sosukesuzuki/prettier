cff2021ca3c180c17bb1ecbb3798492796d14d21
sosukesuzuki <aosukeke@gmail.com>
2021-05-24 00:28:18 +0900

    Add tests

---
+++ tests/format/typescript/argument-expansion/issue-10923.ts
@@ -0,0 +1,21 @@
+longfunctionWithCallBack(
+  "blabla",
+  (thing: string): complex<type<something>> => {
+    code();
+  },
+  foobarbazblablabla
+);
+
+loooong("/", 3, (): a | b | c => {});
+
+loooong("/", 3, (): complex<type<something>> => {});
+
+loooong("/", looooooooooooooooooooooooooong, (): complex<type<something>> => {});
+
+longfunctionWithCallBack(
+  "blabla",
+  foobarbazblablabla,
+  (thing: string): complex<type<something>> => {
+    code();
+  }
+);
